4ab37042e601c55dac0054ae236e8b765fb235eb
jeffersonp317 <jeffersonp317@gmail.com>
2021-06-12 22:56:08 -0300

    Criação da tabela controle financeiro

--- DATABASE/controle_financeiro.sql
+++ DATABASE/controle_financeiro.sql
@@ -1,3 +1,13 @@
 CREATE DATABASE FINANCEIRO
 DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_general_ci;
+
+CREATE TABLE Controle_financeiro (
+id int NOT NULL AUTO_INCREMENT,
+transacao enum('compra', 'venda') DEFAULT NULL,
+produto varchar (255) NOT NULL,
+valor varchar (11) NOT NULL,
+reg datetime DEFAULT NULL,
+PRIMARY KEY (id)
+) DEFAULT CHARSET = utf8;
+
