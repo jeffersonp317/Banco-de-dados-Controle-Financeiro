c75381f3e488f71d619330a17a5ffd3b01ee5f9b
jeffersonp317 <jeffersonp317@gmail.com>
2021-06-13 20:56:12 -0300

    Atualição do banco de dados e inserção dos dados de transação na tabela Controle financeiro.

--- DATABASE/controle_financeiro.sql
+++ DATABASE/controle_financeiro.sql
@@ -6,8 +6,15 @@ CREATE TABLE Controle_financeiro (
 id int NOT NULL AUTO_INCREMENT,
 transacao enum('compra', 'venda') DEFAULT NULL,
 produto varchar (255) NOT NULL,
-valor varchar (11) NOT NULL,
+valor decimal (11,2) NOT NULL,
 reg datetime DEFAULT NULL,
 PRIMARY KEY (id)
 ) DEFAULT CHARSET = utf8;
+
+INSERT INTO Controle_financeiro VALUES
+(DEFAULT,'venda','bola','45.00','2021-06-13 20:39:30'),
+(DEFAULT,'compra','camisa','35.00','2021-06-13 20:40:30'),
+(DEFAULT,'compra','calça','60.00','2021-06-13 20:41:30');
+
+select * from Controle_financeiro
 
